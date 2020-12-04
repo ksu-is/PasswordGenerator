acd1570a62a7eef69763707d3ea76c12ffabb9e9
yaritzahdz <69774155+yaritzahdz@users.noreply.github.com>
2020-12-03 23:57:38 -0500

    Update passwordgen.py
    Added more details and modified output code

--- passwordgen.py
+++ passwordgen.py
@@ -7,17 +7,18 @@ small = ['a','b','c','d','e','z','o','t','h','l','x','w']
 numbers = ['1','2','3','4','5','6','7','8','9','0']
 chars = ['!','@','#','$','*','|','%']
 
+#combined list and made it as one 
+characters= small + numbers + chars + alphabets
  
-ran_1  = random.choice(small)  + str(random.choice(numbers)) + random.choice(chars) + random.choice(alphabets)
-ran_2 = random.choice(small)  + str(random.choice(numbers)) + random.choice(chars) + random.choice(alphabets)
- 
-pass_length = int(input("Desired length of password (4 or 8 characters long): "))
+#modified user input
+pass_length = int(input("Desired length of password: "))
 
-if pass_length <= 4:
-    for i in range(pass_length):
-        password=ran_1
-elif pass_length >= 5:
-    for i in range(pass_length):
-        password = ran_1+ran_2
+#adjusted output to meet specific length
+password=""
+for x in range(pass_length):
+    password = password + random.choice(characters)
+   
+for x in password:
+    break
  
 print("your random password is:", password)
